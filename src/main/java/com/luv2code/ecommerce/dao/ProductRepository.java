4b9380bfffc4eeb2463041a3e088e49f562d006d
yohannes <yohannesbayem@gmail.com>
2021-02-11 17:01:00 -0600

    update

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.ecommerce.dao;
+
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,52 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name= "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_itock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+
+}
